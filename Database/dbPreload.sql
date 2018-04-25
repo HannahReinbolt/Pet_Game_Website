5e2142145957199acef407404981a89a4e648164
SirDalyn <wernergwen@gmail.com>
2018-04-25 17:23:53 -0500

    Sprites! :D (and inserts)

--- Database/dbPreload.sql
+++ Database/dbPreload.sql
@@ -11,3 +11,48 @@ insert into USER values ('SirDalyn', 'StrongPassword', 'Sir', 'Gwen', 'Dalyn', '
 insert into USER2 values ('SirDalyn', 'StrongPassword', '3141592653', 'gmw7x9@mst.edu');
 insert into ADMIN values ('SirDalyn', 'StrongPassword');
 insert into CUSTOMER values ('SirDalyn', 'StrongPassword', 0); 
+
+
+insert into PET values ('001', 'Kitty', 50, 'Sprites/kitty.png');
+insert into PET values ('002', 'Buzz', 100, 'Sprites/bee.png');
+insert into PET values ('003', 'Bunbun', 100, 'Sprites/bunbun.png');
+insert into PET values ('004', 'Corgo', 150, 'Sprites/corgo.png');
+insert into PET values ('005', 'Dino', 200, 'Sprites/dino.png');
+insert into PET values ('006', 'Doggo', 50, 'Sprites/doggo.png');
+insert into PET values ('007', 'Dragon', 300, 'Sprites/dragon.png');
+insert into PET values ('008', 'Toot Toot', 150, 'Sprites/elephant.png');
+insert into PET values ('009', 'Fox', 200, 'Sprites/foxx.png');
+insert into PET values ('010', 'Ladybug', 100, 'Sprites/ladybug.png');
+insert into PET values ('011', 'Squeeker', 100, 'Sprites/mouse.png');
+insert into PET values ('012', 'Panda', 200, 'Sprites/panda.png');
+insert into PET values ('013', 'Piggy', 100, 'Sprites/piggy.png');
+insert into PET values ('014', 'Potato', 0, 'Sprites/potato.png');
+insert into PET values ('015', 'Not Spyro', 300, 'Sprites/purpleboi.png');
+insert into PET values ('016', 'Snek', 200, 'Sprites/snek.png');
+insert into PET values ('017', 'Blue Spiky Boi', 100, 'Sprites/spikyboy.png');
+insert into PET values ('018', 'Unicorn', 300, 'Sprites/unicorn.png');
+insert into PET values ('019', 'Awooo', 100, 'Sprites/wolf.png');
+insert into PET values ('020', 'Zebra', 100, 'Sprites/zebra.png');
+
+insert into COSMETIC values ('101', 'Rainbow', 50, 'Sprites/rainbow.png')
+insert into COSMETIC values ('102', 'Angel', 100, 'Sprites/angel.png')
+insert into COSMETIC values ('103', 'Balloons', 150, 'Sprites/balloons.png')
+insert into COSMETIC values ('104', 'Bitcoim', 125, 'Sprites/bitcoin.png')
+insert into COSMETIC values ('105', 'Burger', 200, 'Sprites/burger.png')
+insert into COSMETIC values ('106', 'Cape', 50, 'Sprites/cape.png')
+insert into COSMETIC values ('107', 'Cowboi', 100, 'Sprites/cowboi.png')
+insert into COSMETIC values ('108', 'Cowboiii', 200, 'Sprites/cowboiii.png')
+insert into COSMETIC values ('109', 'Cutie Mark', 50, 'Sprites/cutiemark.png')
+insert into COSMETIC values ('110', 'Donut', 100, 'Sprites/donut.png')
+insert into COSMETIC values ('111', 'Flower', 50, 'Sprites/flower.png')
+insert into COSMETIC values ('112', 'Im Gayyy', 50, 'Sprites/gayyyy.png')
+insert into COSMETIC values ('113', 'Helicopter', 100, 'Sprites/heli.png')
+insert into COSMETIC values ('114', 'Phone', 100, 'Sprites/phone.png')
+insert into COSMETIC values ('115', 'Pumpkin Head', 100, 'Sprites/pumpkin.png')
+insert into COSMETIC values ('116', 'Rider', 100, 'Sprites/rider.png')
+insert into COSMETIC values ('117', 'Snek Rider', 200, 'Sprites/ridersnek.png')
+insert into COSMETIC values ('118', 'Skateboard', 150, 'Sprites/skateboard.png')
+insert into COSMETIC values ('119', 'Stars', 300, 'Sprites/stars.png')
+insert into COSMETIC values ('120', 'Tiedye', 50, 'Sprites/tiedye.png')
+insert into COSMETIC values ('121', 'Tophat', 100, 'Sprites/tophat.png')
+insert into COSMETIC values ('122', 'Umbrella', 100, 'Sprites/umbrella.png')
