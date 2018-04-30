cd1b80d05112227dcca61a43ba4ea28b28e2fb7c
SirDalyn <wernergwen@gmail.com>
2018-04-29 21:02:05 -0500

    Made the store work

--- Database/dbPreload.sql
+++ Database/dbPreload.sql
@@ -1,13 +1,13 @@
 /* This sql file loads in some default values into our database. Add yourself as admin */
 
-insert into USER values ('frodoHackins', 'toor', 'Frodo', null, 'Hackins', '1998-02-26', 999, null);
+insert into USER values ('frodoHackins', 'toor', 'Frodo', null, 'Hackins', '1998-02-26', 999, '/Sprites/potato.png');
 insert into USER2 values ('frodoHackins', 'toor', '5736664200', 'svgntc@mst.edu');
 insert into ADMIN values ('frodoHackins', 'toor');
 insert into CUSTOMER values ('frodoHackins', 'toor', 3);
 insert into BAN values ('frodoHackins', 'toor', '01-01-9999');
 insert into BAN2 values ('frodoHackins', 'toor', 'everything o_O');
 
-insert into USER values ('SirDalyn', 'StrongPassword', 'Sir', 'Gwen', 'Dalyn', '1996-07-28', 420, null);
+insert into USER values ('SirDalyn', 'StrongPassword', 'Sir', 'Gwen', 'Dalyn', '1996-07-28', 420, '/Sprites/potato.png');
 insert into USER2 values ('SirDalyn', 'StrongPassword', '3141592653', 'gmw7x9@mst.edu');
 insert into ADMIN values ('SirDalyn', 'StrongPassword');
 insert into CUSTOMER values ('SirDalyn', 'StrongPassword', 0); 
@@ -29,7 +29,7 @@ insert into PET values ('013', 'Piggy', 100, 'Sprites/piggy.png');
 insert into PET values ('014', 'Potato', 0, 'Sprites/potato.png');
 insert into PET values ('015', 'Not Spyro', 300, 'Sprites/purpleboi.png');
 insert into PET values ('016', 'Snek', 200, 'Sprites/snek.png');
-insert into PET values ('017', 'Blue Spiky Boi', 100, 'Sprites/spikyboy.png');
+insert into PET values ('017', 'Blue SpikyBoi', 100, 'Sprites/spikyboy.png');
 insert into PET values ('018', 'Unicorn', 300, 'Sprites/unicorn.png');
 insert into PET values ('019', 'Awooo', 100, 'Sprites/wolf.png');
 insert into PET values ('020', 'Zebra', 100, 'Sprites/zebra.png');
